ef7aa46264029feddc3c7a99e743266fbaa33365
Richards Cedeño <chards.rjcov@gmail.com>
2022-10-09 01:25:19 -0500

    first commit

---
+++ android/app/src/main/java/com/josecedeno/appfeed/MainActivity.java
@@ -0,0 +1,5 @@
+package com.josecedeno.appfeed;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.josecedeno.appfeed',
+  appName: 'appFeed',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
